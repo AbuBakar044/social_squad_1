aeeea6516eb00f04a758ee417da06d39e8d9abe0
AbuBakar044 <bhattiabubakar@gmail.com>
2024-05-14 17:10:52 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/social_squad_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.social_squad_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/splash_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Center(
+          child: Image.asset(
+            'assets/images/logo.jpg',
+            height: 200,
+            width: 200,
+          ),
+        ),
+      ),
+    );
+  }
+}
